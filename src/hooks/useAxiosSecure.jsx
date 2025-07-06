572298a50c41afcf630b324bc8a7e17d6479a81e
Mysterio-O <skrabbi.019@gmail.com>
2025-07-06 15:45:17 +0600

    added response interceptors to provide more security to the admin routes

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,17 +1,41 @@
 import axios from "axios";
 import useAuth from "./useAuth";
+import { useNavigate } from "react-router";
 
 const axiosSecure = axios.create({
-    baseURL:`http://localhost:3000`
+    baseURL: `http://localhost:3000`
 });
 
-const useAxiosSecure = ()=> {
+const useAxiosSecure = () => {
 
-    const {user} = useAuth();
+    const { user, userLogOut } = useAuth();
+
+    const navigate = useNavigate();
 
     axiosSecure.interceptors.request.use(config => {
         config.headers.Authorization = `Bearer ${user.accessToken}`
         return config
+    }, error => {
+        return Promise.reject(error);
+    });
+
+
+    axiosSecure.interceptors.response.use(res => {
+        return res;
+    }, error => {
+        const status = error?.status;
+        if (status === 403) {
+            navigate('/forbidden');
+        } else if (status === 401) {
+            userLogOut()
+                .then(() => {
+                    navigate('/login');
+                })
+                .catch(err => {
+                    console.log('error from response interceptor in axiosSecure', err);
+                })
+        };
+        Promise.reject(error);
     })
 
     return axiosSecure;
